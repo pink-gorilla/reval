7c84efea8c7ee859467911dc5ebf0bbcebb9b806
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-11-04 02:43:56 +0100

    ui lib ns change

--- src/goldly/lib/menu.cljs
+++ src/goldly/lib/menu.cljs
@@ -1,5 +1,16 @@
 
 
+(defn link-fn [fun text]
+  [:a.bg-blue-600.cursor-pointer.hover:bg-red-700.m-1
+   {:on-click fun} text])
+
+(defn link-dispatch [rf-evt text]
+  (link-fn #(rf/dispatch rf-evt) text))
+
+(defn link-href [href text]
+  [:a.bg-blue-600.cursor-pointer.hover:bg-red-700.m-1
+   {:href href} text])
+
 (defn devtools-menu []
   [:div
    [link-dispatch [:bidi/goto :viewer :query-params {}] "notebook viewer"]
--- src/goldly/lib/util.cljs
+++ src/goldly/lib/util.cljs
@@ -1,14 +1,5 @@
 
-(defn link-fn [fun text]
-  [:a.bg-blue-600.cursor-pointer.hover:bg-red-700.m-1
-   {:on-click fun} text])
 
-(defn link-dispatch [rf-evt text]
-  (link-fn #(rf/dispatch rf-evt) text))
-
-(defn link-href [href text]
-  [:a.bg-blue-600.cursor-pointer.hover:bg-red-700.m-1
-   {:href href} text])
 
 (defn rdoc-link [ns name]
   (str "/api/rdocument/file/" ns "/" name))
