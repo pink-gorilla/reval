511abc850ca1feff81c6aee044a54d240f418008
awb99 <andreas.wolfgang.bauer@gmail.com>
2021-11-26 15:29:29 +0100

    task config

--- src/reval/task.clj
+++ src/reval/task.clj
@@ -2,10 +2,11 @@
   (:require
    [taoensso.timbre :refer [debug info warnf error]]
    [modular.log :refer [timbre-config!]]
-   [modular.config :refer [config-atom get-in-config]]
+   [modular.config :refer [config-atom get-in-config load-config!]]
    [reval.document.collection  :refer [nb-collections eval-collections]]
    [reval.default] ; side-effects
    ))
+
 (def default-reval-config
   {:timbre-loglevel [[#{"*"} :info]]
    :reval {:rdocument {:storage-root "demo/rdocument/"
@@ -14,21 +15,18 @@
                          :demo [:clj "demo/notebook/"]
                          :demo-cljs [:cljs "demo/notebook/"]}}})
 
-(defn init-config! []
-  (if (get-in-config [:reval])
-    (info "using user config")
-    (do
-      (info "using default config!")
-      (reset! config-atom default-reval-config)
-      (timbre-config! @config-atom))))
-
 (defn nbeval
-  ([config]
-   (init-config!)
+  ([{:keys [config]; a map so it can be consumed by tools deps -X
+     :or {config default-reval-config}
+     :as p}]
+   (if p
+     (do (info "using user config")
+         (load-config! config))
+     (do (info "using default config!")
+         (reset! config-atom default-reval-config)))
+   (timbre-config! @config-atom)
    (nbeval))
   ([]
    (-> (nb-collections)
        eval-collections)))
-
-
 
