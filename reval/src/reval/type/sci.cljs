529434f3a460ef2f1e78aebcc042f03d32f520c2
awb99 <andreas.wolfgang.bauer@gmail.com>
2022-06-10 06:34:37 +0200

    removed sci-var sci-ns

--- reval/src/reval/type/sci.cljs
+++ reval/src/reval/type/sci.cljs
@@ -1,17 +1,17 @@
 (ns reval.type.sci
   "converts sci values to hiccup representation"
   (:require
-   [sci.impl.vars :refer [SciNamespace]]
+   ;[sci.impl.vars]
    [reval.type.protocol :refer [hiccup-convertable #_to-hiccup]]
    [reval.type.ui.simplevalue :refer [simplevalue->hiccup]]
    [reval.type.ui.list :refer [list->hiccup map->hiccup]]))
 
-(extend-type sci.impl.vars/SciVar
+#_(extend-type sci.impl.vars/SciVar
   hiccup-convertable
   (to-hiccup [self]
     (simplevalue->hiccup self "clj-symbol")))
 
-(extend-type sci.impl.vars.SciNamespace
+#_(extend-type sci.impl.vars.SciNamespace
   hiccup-convertable
   (to-hiccup [self]
     (simplevalue->hiccup self "clj-namespace")))
