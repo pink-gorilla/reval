cea44a2d8de9cc5ec7643d6026cfbdc09f6a2ac3
awb99 <awb99@gmail.com>
2024-04-22 20:48:00 -0500

    css cleanup

--- reval-ui/build.clj
+++ reval-ui/build.clj
@@ -1,8 +1,5 @@
 (ns build
   (:require
-   [babashka.fs :as fs]
-   [clojure.java.io :as io]
-   [clojure.string :as str]
    [clojure.tools.build.api :as b]
    [org.corfield.build :as bb] ; https://github.com/seancorfield/build-clj
    ))
