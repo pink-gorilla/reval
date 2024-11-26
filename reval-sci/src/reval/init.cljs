da83efa5198ed9239b879868c5e3029b65cb5f96
awb99 <awb99@gmail.com>
2024-11-26 00:56:55 -0500

    reval init bugfix

--- reval-sci/src/reval/init.cljs
+++ reval-sci/src/reval/init.cljs
@@ -3,8 +3,8 @@
    [taoensso.timbre :as timbre :refer [info]]
    [webly.module.build :refer [load-namespace]]))
 
-(defn reval-cljs-kernel-init []
-  (info "goldly reval cljs-kernel-init ..")
+(defn reval-cljs-kernel-init [_config]
+  (info "reval cljs-kernel-init ..")
   (load-namespace 'reval.kernel.protocol)
   (load-namespace 'reval.kernel.cljs-sci)
   nil)
